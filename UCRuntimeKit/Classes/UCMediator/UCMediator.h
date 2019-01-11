26743a475568ec4836558138e9417e1154045386
Link <fanyang_32012@outlook.com>
2019-01-11 18:48:22 +0800

    nativePerformTarget unit test case complete

--- UCRuntimeKit/Classes/UCMediator/UCMediator.h
+++ UCRuntimeKit/Classes/UCMediator/UCMediator.h
@@ -21,12 +21,14 @@ NS_ASSUME_NONNULL_BEGIN
 
 /**
  第三方，以及外界app通过url调用
+ 调用这个api,只会给方法的第一个参数传智,接收的参数类型在运行时是UCMediatorArgument
  */
 - (nullable id)thirdPartyPerformActionWithUrl:(nonnull NSString *)urlStr
                                    completion:(nullable void (^)(NSDictionary *result))completion;
 
 /**
  本地url调用，不推荐，内部可以解析url以及参数，推荐内部调用使用下面的利用字符串调用
+ 调用这个api,只会给方法的第一个参数传智,接收的参数类型在运行时是UCMediatorArgument
  */
 - (nullable id)nativePerformActionWithUrl:(nonnull NSString *)urlStr
                                       arg:(nullable NSDictionary *)arg
@@ -35,6 +37,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 /**
  本地利用字符串调用，推荐，内部也做了处理
+ 调用这个api,只会给方法的第一个参数传智,接收的参数类型在运行时是UCMediatorArgument
  */
 - (nullable id)nativePerformTarget:(nonnull NSString *)targetName
                             action:(nonnull NSString *)actionName
@@ -44,6 +47,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 /**
  根据字符串动态调用各个模块的appdelegate方法
+ 支持多参数,这里的参数只做转发不校验
  */
 - (BOOL)performAppDelegateTarget:(nonnull NSString *)targetName
                       actionName:(nonnull NSString *)actionName
@@ -51,6 +55,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 /**
  直接向这个对象动态调用appdelegate的方法
+ 支持多参数,这里的参数只做转发不校验
  */
 - (BOOL)performAppDelegateTargetObject:(nonnull id)targetObj
                             actionName:(nonnull NSString *)actionName
@@ -59,6 +64,7 @@ NS_ASSUME_NONNULL_BEGIN
 #pragma mark - 异常处理
 /**
  第三方，以及外界app通过url调用,包含了error处理
+ 调用这个api,只会给方法的第一个参数传智,接收的参数类型在运行时是UCMediatorArgument
  */
 - (nullable id)thirdPartyPerformActionWithUrl:(nonnull NSString *)urlStr
                                    completion:(nullable void (^)(NSDictionary *result))completion
@@ -66,6 +72,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 /**
  本地url调用，不推荐，内部可以解析url以及参数，推荐内部调用使用下面的利用字符串调用,包含了error处理
+ 调用这个api,只会给方法的第一个参数传智,接收的参数类型在运行时是UCMediatorArgument
  */
 - (nullable id)nativePerformActionWithUrl:(nonnull NSString *)urlStr
                                       arg:(nullable NSDictionary *)arg
@@ -75,6 +82,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 /**
  本地利用字符串调用，推荐，内部也做了处理,包含了error处理
+ 调用这个api,只会给方法的第一个参数传智,接收的参数类型在运行时是UCMediatorArgument
  */
 - (nullable id)nativePerformTarget:(nonnull NSString *)targetName
                             action:(nonnull NSString *)actionName
@@ -85,6 +93,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 /**
  根据字符串动态调用各个模块的appdelegate方法,包含了error处理
+ 支持多参数,这里的参数只做转发不校验
  */
 - (BOOL)performAppDelegateTarget:(nonnull NSString *)targetName
                       actionName:(nonnull NSString *)actionName
@@ -92,7 +101,8 @@ NS_ASSUME_NONNULL_BEGIN
                            error:(NSError **)error;
 
 /**
- 直接向这个对象动态调用appdelegate的方法
+ 直接向这个对象动态调用appdelegate的方法,包含了error处理
+ 支持多参数,这里的参数只做转发不校验
  */
 - (BOOL)performAppDelegateTargetObject:(nonnull id)targetObj
                             actionName:(nonnull NSString *)actionName
--- UCRuntimeKit/Classes/UCMediator/UCMediator.m
+++ UCRuntimeKit/Classes/UCMediator/UCMediator.m
@@ -29,6 +29,7 @@
 
 /**
  第三方，以及外界app通过url调用
+ 调用这个api,只会给方法的第一个参数传智,接收的参数类型在运行时是UCMediatorArgument
  */
 - (nullable id)thirdPartyPerformActionWithUrl:(nonnull NSString *)urlStr
                                    completion:(nullable void (^)(NSDictionary *result))completion {
@@ -37,17 +38,19 @@
 
 /**
  本地url调用，不推荐，内部可以解析url以及参数，推荐内部调用使用下面的利用字符串调用
+ 调用这个api,只会给方法的第一个参数传智,接收的参数类型在运行时是UCMediatorArgument
  */
 - (nullable id)nativePerformActionWithUrl:(nonnull NSString *)urlStr
                                       arg:(nullable NSDictionary *)arg
                                completion:(nullable void (^)(NSDictionary *result))completionCallBack
                                   failure:(nullable void (^)(NSError *error))failureCallBack {
-
+    
     return [self nativePerformActionWithUrl:urlStr arg:arg completion:completionCallBack failure:failureCallBack error:nil];
 }
 
 /**
  本地利用字符串调用，推荐，内部也做了处理
+ 调用这个api,只会给方法的第一个参数传智,接收的参数类型在运行时是UCMediatorArgument
  */
 - (nullable id)nativePerformTarget:(nonnull NSString *)targetName
                             action:(nonnull NSString *)actionName
@@ -60,16 +63,18 @@
 
 /**
  动态调用各个模块的appdelegate方法
+ 支持多参数,这里的参数只做转发不校验
  */
 - (BOOL)performAppDelegateTarget:(nonnull NSString *)targetName
                       actionName:(nonnull NSString *)actionName
                           params:(nonnull NSArray<id> *)paramsArray {
-
+    
     return [self performAppDelegateTarget:targetName actionName:actionName params:paramsArray error:nil];
 }
 
 /**
  直接向这个对象动态调用appdelegate的方法
+ 支持多参数,这里的参数只做转发不校验
  */
 - (BOOL)performAppDelegateTargetObject:(nonnull id)targetObj
                             actionName:(nonnull NSString *)actionName
@@ -145,9 +150,6 @@
     }
 }
 
-/**
- 本地利用字符串调用，推荐，内部也做了处理,包含了error处理
- */
 - (nullable id)nativePerformTarget:(nonnull NSString *)targetName
                             action:(nonnull NSString *)actionName
                             params:(nullable NSDictionary *)params
@@ -204,7 +206,9 @@
     
     if (![actionName containsString:@"application"]) {
         UCLog(@"检查调用action,本方法仅用来派发appdelegate的各个方法");
-        *error = UCMediatorErrorWithAppdelegateRuntimeInvoke();
+        if (error != NULL) {
+            *error = UCMediatorErrorWithAppdelegateRuntimeInvoke();
+        }
         return NO;
     }
     
@@ -226,15 +230,21 @@
     NSString *errorStr;
     if (!target) {
         errorStr = [NSString stringWithFormat:@"*%@组件不存在, 请检查调用", target];
-        *error = UCMediatorErrorWithUndefindTarget();
+        if (error != NULL) {
+            *error = UCMediatorErrorWithUndefindTarget();
+        }
     } else if (!action) {
         NSString *actionStr = NSStringFromSelector(action);
         errorStr = [NSString stringWithFormat:@"*%@组件%@方法调用错误, 请检查调用", target, actionStr];
-        *error = UCMediatorErrorWithUndefindSelctor();
+        if (error != NULL) {
+            *error = UCMediatorErrorWithUndefindSelctor();
+        }
     } else if (![[target new] respondsToSelector:action]) {
         NSString *actionStr = NSStringFromSelector(action);
         errorStr = [NSString stringWithFormat:@"*%@组件没有实现%@方法, 请检查调用", target, actionStr];
-        *error = UCMediatorErrorWithUndefindSelctor();
+        if (error != NULL) {
+            *error = UCMediatorErrorWithUndefindSelctor();
+        }
     }
     UCLog(@"%@", errorStr);
     return errorStr.length == 0;
@@ -244,10 +254,12 @@
                                               completion:(nullable void (^)(NSDictionary *result))completionCallBack
                                                  failure:(nullable void (^)(NSError *error))failureCallBack
                                                    error:(NSError **)error {
-
-    if (![arg isKindOfClass:[NSDictionary class]]) {
+    
+    if (arg != nil && ![arg isKindOfClass:[NSDictionary class]]) {
         UCLog(@"参数类型错误,只能穿入字典!请仔细检查.参数:%@, 参数类型:%@", arg, NSStringFromClass([arg class]));
-        *error = UCMediatorErrorWithParms();
+        if (error != NULL) {
+            *error = UCMediatorErrorWithParms();
+        }
         return nil;
     }
     
@@ -257,14 +269,14 @@
     }else{
         argObj.arguDict = nil;
     }
-
+    
     if (completionCallBack) {
         argObj.completionCallBack = completionCallBack;
     }else{
         void(^tempBlock)(NSDictionary *) = ^(NSDictionary *result){};
         argObj.completionCallBack = tempBlock;
     }
-
+    
     if (failureCallBack) {
         argObj.failureCallBack = failureCallBack;
     }else{
@@ -281,7 +293,9 @@
     NSMethodSignature* methodSig = [target methodSignatureForSelector:action];
     if(methodSig == nil) {
         UCLog(@"函数签名异常");
-        *error = UCMediatorErrorWithUndefindMethodSignature();
+        if (error != NULL) {
+            *error = UCMediatorErrorWithUndefindMethodSignature();
+        }
         return nil;
     }
     const char* retType = [methodSig methodReturnType];
@@ -292,14 +306,16 @@
     
     //参数处理
     NSUInteger paramsCount = [selStr componentsSeparatedByString:@":"].count - 1;
-    if (paramsCount >= 1 && paramsCount == paramsArray.count) {
+    if (paramsCount > 0 && paramsCount >= paramsArray.count) {
         for (int i = 0; i < paramsArray.count; i ++) {
             id param = paramsArray[i];
             [invocation setArgument:&param atIndex:i + 2];
         }
-    } else if (paramsCount != paramsArray.count) {
+    } else if (paramsCount > 0 && paramsCount < paramsArray.count) {
         UCLog(@"runtime参数数量与SEL所需数量异常");
-        *error = UCMediatorErrorWithRuntimeParams();
+        if (error != NULL) {
+            *error = UCMediatorErrorWithRuntimeParams();
+        }
         return nil;
     }
     
@@ -309,6 +325,69 @@
     if (strcmp(retType, @encode(void)) == 0) {
         [invocation invoke];
         return nil;
+    }
+    
+    if (strcmp(retType, @encode(int)) == 0) {
+        [invocation invoke];
+        int result = 0;
+        [invocation getReturnValue:&result];
+        return @(result);
+    }
+    
+    if (strcmp(retType, @encode(unsigned int)) == 0) {
+        [invocation invoke];
+        unsigned int result = 0;
+        [invocation getReturnValue:&result];
+        return @(result);
+    }
+    
+    if (strcmp(retType, @encode(short)) == 0) {
+        [invocation invoke];
+        short result = 0;
+        [invocation getReturnValue:&result];
+        return @(result);
+    }
+    
+    if (strcmp(retType, @encode(unsigned short)) == 0) {
+        [invocation invoke];
+        unsigned short result = 0;
+        [invocation getReturnValue:&result];
+        return @(result);
+    }
+    
+    if (strcmp(retType, @encode(long)) == 0) {
+        [invocation invoke];
+        long result = 0;
+        [invocation getReturnValue:&result];
+        return @(result);
+    }
+    
+    if (strcmp(retType, @encode(long long)) == 0) {
+        [invocation invoke];
+        long long result = 0;
+        [invocation getReturnValue:&result];
+        return @(result);
+    }
+    
+    if (strcmp(retType, @encode(float)) == 0) {
+        [invocation invoke];
+        float result = 0.0f;
+        [invocation getReturnValue:&result];
+        return @(result);
+    }
+    
+    if (strcmp(retType, @encode(char)) == 0) {
+        [invocation invoke];
+        char result = 0;
+        [invocation getReturnValue:&result];
+        return @(result);
+    }
+    
+    if (strcmp(retType, @encode(unsigned char)) == 0) {
+        [invocation invoke];
+        unsigned char result = 0;
+        [invocation getReturnValue:&result];
+        return @(result);
     }
     
     if (strcmp(retType, @encode(NSInteger)) == 0) {

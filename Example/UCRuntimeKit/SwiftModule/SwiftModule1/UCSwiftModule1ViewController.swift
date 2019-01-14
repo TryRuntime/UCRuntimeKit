371689f7b4f7b49742f6346adbac4ef5e607a9d0
Link <fanyang_32012@outlook.com>
2019-01-14 14:27:25 +0800

    新建单元测试文件

---
+++ Example/UCRuntimeKit/SwiftModule/SwiftModule1/UCSwiftModule1ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  UCSwiftModule1ViewController.swift
+//  UCRuntimeKit
+//
+//  Created by Link913 on 2019/1/14.
+//  Copyright © 2019 Link. All rights reserved.
+//
+
+import UIKit
+
+class UCSwiftModule1ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}

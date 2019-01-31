f597c3007e0d7333da5c055b6530ff9f6c1aa9eb
Link <fanyang_32012@outlook.com>
2019-01-31 14:24:10 +0800

    swift -> category -> Objc单元测试完成

--- Example/Tests/UCMediatorSwiftTest/UCMediatorSwiftCategoryObjcTest.swift
+++ Example/Tests/UCMediatorSwiftTest/UCMediatorSwiftCategoryObjcTest.swift
@@ -18,11 +18,6 @@ class UCMediatorSwiftCategoryObjcTest: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
@@ -30,4 +25,275 @@ class UCMediatorSwiftCategoryObjcTest: XCTestCase {
         }
     }
 
+    func testNoArguNoReturn() {
+        UCMediator.sharedInstance().testSwiftCategoryObjcNoArguNoReturn { (error) in
+            XCTAssertNil(error, "error code = \(error._code)")
+        }
+    }
+    
+    func testOneArguNoReturn() {
+        UCMediator.sharedInstance().testSwiftCategoryObjcOneArguNoReturn { (error) in
+            XCTAssertNil(error, "error code = \(error._code)")
+        }
+    }
+    
+    func testNoArguReturnInt() {
+
+        let result = UCMediator.sharedInstance().testSwiftCategoryObjcNoArguReturnInt { (error) in
+            XCTAssertNil(error, "error code = \(error._code)")
+        }
+
+        guard let number = result as? Int, number == 10 else {
+            XCTFail("返回值异常")
+            return
+        }
+    }
+    
+    func testNoArguReturnUInt() {
+        
+        let result = UCMediator.sharedInstance().testSwiftCategoryObjcNoArguReturnUInt { (error) in
+            XCTAssertNil(error, "error code = \(error._code)")
+        }
+        
+        guard let number = result as? uint, number == 14 else {
+            XCTFail("返回值异常")
+            return
+        }
+    }
+    
+    func testNoArguReturnShort() {
+        
+        let result = UCMediator.sharedInstance().testSwiftCategoryObjcNoArguReturnShort { (error) in
+            XCTAssertNil(error, "error code = \(error._code)")
+        }
+        
+        guard let number = result as? CShort, number == 11 else {
+            XCTFail("返回值异常")
+            return
+        }
+    }
+    
+    func testNoArguReturnUShort() {
+        
+        let result = UCMediator.sharedInstance().testSwiftCategoryObjcNoArguReturnUShort { (error) in
+            XCTAssertNil(error, "error code = \(error._code)")
+        }
+        
+        guard let number = result as? CUnsignedShort, number == 12 else {
+            XCTFail("返回值异常")
+            return
+        }
+    }
+    
+    func testNoArguReturnLong() {
+        
+        let result = UCMediator.sharedInstance().testSwiftCategoryObjcNoArguReturnLong { (error) in
+            XCTAssertNil(error, "error code = \(error._code)")
+        }
+        
+        guard let number = result as? CLong, number == 12 else {
+            XCTFail("返回值异常")
+            return
+        }
+    }
+    
+    func testNoArguReturnULong() {
+        
+        let result = UCMediator.sharedInstance().testSwiftCategoryObjcNoArguReturnULong { (error) in
+            XCTAssertNil(error, "error code = \(error._code)")
+        }
+        
+        guard let number = result as? CUnsignedLong, number == 17 else {
+            XCTFail("返回值异常")
+            return
+        }
+    }
+    
+    func testNoArguReturnLongLong() {
+        
+        let result = UCMediator.sharedInstance().testSwiftCategoryObjcNoArguReturnLongLong { (error) in
+            XCTAssertNil(error, "error code = \(error._code)")
+        }
+        
+        guard let number = result as? CLongLong, number == 16 else {
+            XCTFail("返回值异常")
+            return
+        }
+    }
+    
+    func testNoArguReturnULongLong() {
+        
+        let result = UCMediator.sharedInstance().testSwiftCategoryObjcNoArguReturnULongLong { (error) in
+            XCTAssertNil(error, "error code = \(error._code)")
+        }
+        
+        guard let number = result as? CUnsignedLongLong, number == 15 else {
+            XCTFail("返回值异常")
+            return
+        }
+    }
+    
+    func testNoArguReturnFloat() {
+        
+        let result = UCMediator.sharedInstance().testSwiftCategoryObjcNoArguReturnFloat { (error) in
+            XCTAssertNil(error, "error code = \(error._code)")
+        }
+        
+        guard let number = result as? Float, number == 0.1 else {
+            XCTFail("返回值异常")
+            return
+        }
+    }
+    
+    func testNoArguReturnDouble() {
+        
+        let result = UCMediator.sharedInstance().testSwiftCategoryObjcNoArguReturnDouble { (error) in
+            XCTAssertNil(error, "error code = \(error._code)")
+        }
+        
+        //精度j丢失
+        guard let number = result as? Double, fabs(number - 0.1) < 0.000001 else {
+            XCTFail("返回值异常")
+            return
+        }
+    }
+    
+    func testNoArguReturnChar() {
+        
+        let result = UCMediator.sharedInstance().testSwiftCategoryObjcNoArguReturnChar { (error) in
+            XCTAssertNil(error, "error code = \(error._code)")
+        }
+        
+        guard let number = result as? char16_t, number == 97 else {
+            XCTFail("返回值异常")
+            return
+        }
+    }
+    
+    func testNoArguReturnUChar() {
+        
+        let result = UCMediator.sharedInstance().testSwiftCategoryObjcNoArguReturnUChar { (error) in
+            XCTAssertNil(error, "error code = \(error._code)")
+        }
+        
+        guard let number = result as? CUnsignedChar, number == 98 else {
+            XCTFail("返回值异常")
+            return
+        }
+    }
+    
+    func testNoArguReturnTrue() {
+        
+        let result = UCMediator.sharedInstance().testSwiftCategoryObjcNoArguReturnTrue { (error) in
+            XCTAssertNil(error, "error code = \(error._code)")
+        }
+        
+        guard let number = result as? Bool, number == true else {
+            XCTFail("返回值异常")
+            return
+        }
+    }
+    
+    func testNoArguReturnFalse() {
+        
+        let result = UCMediator.sharedInstance().testSwiftCategoryObjcNoArguReturnFalse { (error) in
+            XCTAssertNil(error, "error code = \(error._code)")
+        }
+        
+        guard let number = result as? Bool, number == false else {
+            XCTFail("返回值异常")
+            return
+        }
+    }
+    
+    func testNoArguReturnInteger() {
+        
+        let result = UCMediator.sharedInstance().testSwiftCategoryObjcNoArguReturnInteger { (error) in
+            XCTAssertNil(error, "error code = \(error._code)")
+        }
+        
+        guard let number = result as? Int, number == 1 else {
+            XCTFail("返回值异常")
+            return
+        }
+    }
+    
+    func testNoArguReturnUInteger() {
+        
+        let result = UCMediator.sharedInstance().testSwiftCategoryObjcNoArguReturnUInteger { (error) in
+            XCTAssertNil(error, "error code = \(error._code)")
+        }
+        
+        guard let number = result as? uint, number == 3 else {
+            XCTFail("返回值异常")
+            return
+        }
+    }
+    
+    func testNoArguReturnCGFloat() {
+        
+        let result = UCMediator.sharedInstance().testSwiftCategoryObjcNoArguReturnCGFloat { (error) in
+            XCTAssertNil(error, "error code = \(error._code)")
+        }
+        
+        guard let number = result as? CGFloat, number == 0.1 else {
+            XCTFail("返回值异常")
+            return
+        }
+    }
+    
+    func testNoArguReturnNSString() {
+        
+        let result = UCMediator.sharedInstance().testSwiftCategoryObjcNoArguReturnNSString { (error) in
+            XCTAssertNil(error, "error code = \(error._code)")
+        }
+        
+        guard let number = result as? NSString, number == "a" else {
+            XCTFail("返回值异常")
+            return
+        }
+    }
+    
+    func testNoArguReturnIntArray() {
+        
+        let result = UCMediator.sharedInstance().testSwiftCategoryObjcNoArguReturnIntArray { (error) in
+            XCTAssertNil(error, "error code = \(error._code)")
+        }
+        
+        guard let number = result as? [Int], number == [1, 2, 3] else {
+            XCTFail("返回值异常")
+            return
+        }
+    }
+    
+    func testNoArguReturnNSDictionary() {
+        
+        let result = UCMediator.sharedInstance().testSwiftCategoryObjcNoArguReturnNSDictionary { (error) in
+            XCTAssertNil(error, "error code = \(error._code)")
+        }
+        
+        guard let number = result as? [String: String], number == ["key1": "value"] else {
+            XCTFail("返回值异常")
+            return
+        }
+    }
+    
+    func testNoArguReturnNSNumber() {
+        
+        let result = UCMediator.sharedInstance().testSwiftCategoryObjcNoArguReturnNSNumber { (error) in
+            XCTAssertNil(error, "error code = \(error._code)")
+        }
+        
+        guard let number = result as? NSNumber, number == NSNumber(integerLiteral: 1) else {
+            XCTFail("返回值异常")
+            return
+        }
+    }
+    
+    func testBlock() {
+        
+        UCMediator.sharedInstance().testSwiftCategoryObjcBlock { (error) in
+            XCTAssertNil(error, "error code = \(error._code)")
+        }
+    }
 }
